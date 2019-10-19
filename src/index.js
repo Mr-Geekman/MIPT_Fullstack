c89ed0122b665155b3825618dfaca414975eb6b4
Mr-Geekman <bunin260200@mail.ru>
2019-10-19 19:49:51 +0300

    Removed commented area

--- src/index.js
+++ src/index.js
@@ -1,35 +1,10 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import './index.css';
-// import Header from "./components/header";
-// import Footer from './components/footer';
-// import Main from './components/main'
-// import * as serviceWorker from './serviceWorker';
-//
-// ReactDOM.render(
-//     <React.Fragment>
-//         <Header/>
-//         <Main/>
-//         <Footer/>
-//     </React.Fragment>,
-//     document.getElementById('root')
-// );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-// serviceWorker.unregister();
-
-
-
-
-
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import MainRouter from './routes';
+import * as serviceWorker from './serviceWorker';
+
 const render = () =>
     ReactDOM.render(
             <BrowserRouter>
@@ -38,3 +13,8 @@ const render = () =>
         document.getElementById('root')
     );
 render();
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
