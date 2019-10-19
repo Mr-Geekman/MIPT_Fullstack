159b37af7b65de41bd399c68d69dd75c99c53eb9
Mr-Geekman <bunin260200@mail.ru>
2019-10-19 22:37:28 +0300

    added 404 picture

--- src/components/page_not_found/index.js
+++ src/components/page_not_found/index.js
@@ -1,5 +1,7 @@
 import React, {Component} from 'react';
 import { Link } from "react-router-dom";
+import NotFoundPicture from "../../static/img/404.jpeg"
+import './styles.css';
 
 class PageNotFound extends Component {
 
@@ -9,7 +11,8 @@ class PageNotFound extends Component {
                 <main>
                     <article>
                         <h2>Похоже, что вы забрели не туда, такой страницы не существует</h2>
-                        <p>Советуем вам вернуться обратно на <Link to="/">главную страницу</Link></p>
+                        <img className={"not_found_picture"} src={NotFoundPicture} alt="Bosch Hell"/>
+                        <p className={"tip_to_main"}>Советуем вам вернуться обратно на <Link to="/">главную страницу</Link></p>
                     </article>
                 </main>
             </div>
