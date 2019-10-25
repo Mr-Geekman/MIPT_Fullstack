22267fdf4271f8baf63311b2a916572b15778564
Mr-Geekman <bunin260200@mail.ru>
2019-10-26 01:34:03 +0300

    added map dragging and borders (where are some problems with resizes)

--- src/components/map/index.js
+++ src/components/map/index.js
@@ -1,7 +1,60 @@
 import React, {Component} from 'react';
-import { Stage, Layer, Text } from 'react-konva';
-import PageNotFound from "../../components/page_not_found"
+import { Stage, Layer, Image } from 'react-konva';
+import PageNotFound from "../../components/page_not_found";
 import './styles.css';
+
+
+class MapImage extends React.Component {
+  state = {
+    image: null
+  };
+
+  componentDidMount() {
+    this.loadImage();
+  }
+
+  componentDidUpdate(oldProps) {
+    if (oldProps.src !== this.props.src) {
+      this.loadImage();
+    }
+  }
+
+  componentWillUnmount() {
+    this.image.removeEventListener('load', this.handleLoad);
+  }
+
+  loadImage() {
+    // save to "this" to remove "load" handler on unmount
+    this.image = new window.Image();
+    this.image.src = this.props.src;
+    this.image.addEventListener('load', this.handleLoad);
+  }
+
+  handleLoad = () => {
+    // after setState react-konva will update canvas and redraw the layer
+    // because "image" property is changed
+    this.setState({
+      image: this.image
+    });
+    // if you keep same image object during source updates
+    // you will have to update layer manually:
+    // this.imageNode.getLayer().batchDraw();
+  };
+
+  render() {
+    return (
+      <Image
+        x={this.props.x}
+        y={this.props.y}
+        image={this.state.image}
+        ref={node => {
+          this.imageNode = node;
+        }}
+      />
+    );
+  }
+}
+
 
 class Map extends Component {
     constructor(props) {
@@ -26,11 +79,29 @@ class Map extends Component {
                 <PageNotFound/>
             );
         }
+        let header_height = document.getElementsByTagName('header')[0].clientHeight;
+        let footer_height = document.getElementsByTagName('footer')[0].clientHeight;
+        // Проблема: visible_width и visible_height не обновляются.
+        // Для воспроизведения нужно перезагрузить страницу с открытым режимом разработчика, а затем выклюить его.
+        // В таком случае часть экрана никак не будет заполнена изображением.
+        let visible_width = window.innerWidth;
+        let visible_height = window.innerHeight - header_height - footer_height;
+        let bound_function = function (position) {
+            console.log('called');
+            // Придумать, откуда брать размеры картинки (вариант: redux)
+            let map_width = 10512 - visible_width;
+            let map_height = 5197 - visible_height;
+            let new_x = Math.max(Math.min(0, position.x), -map_width);
+            let new_y = Math.max(Math.min(0, position.y), -map_height);
+            console.log(position.y);
+            console.log('');
+            return {x: new_x, y: new_y};
+        };
         return (
             <main>
-                <Stage width={window.innerWidth} height={window.innerHeight}>
-                    <Layer>
-                        <Text text="Test text" fill="white" fontSize="24" draggable/>
+                <Stage width={visible_width} height={visible_height}>
+                    <Layer draggable dragBoundFunc={bound_function}>
+                        <MapImage src="https://src.lotrrol.ru/complete_map_9.jpg"/>
                     </Layer>
                 </Stage>
             </main>
