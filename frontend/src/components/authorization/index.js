8e8e92f7491889ebc79f869cdc0250ea5e5be7ce
LAPTOP-UUH16LKD\Fedor <fedor.noskov.99@mail.ru>
2019-11-29 03:05:28 +0300

    auth requests

--- frontend/src/components/authorization/index.js
+++ frontend/src/components/authorization/index.js
@@ -1,16 +1,21 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React, { useState, Component } from 'react';
 import { Button, Form, FormGroup, Label, Input, Col } from 'reactstrap';
-import './styles.css'
+import './styles.css';
+import * as Constants from '../../constants/constants'
 
 class AuthorizationForm extends Component {
 
     constructor(props) {
         super(props);
         this.state = {
-            height: 0
+            height: 0,
+            login: '',
+            password: ''
         };
-        this.handleLoad = this.handleLoad.bind(this)
+        this.handleLoad = this.handleLoad.bind(this);
+        this.submitForm = this.submitForm.bind(this);
+        this.handleChange = this.handleChange.bind(this);
     }
 
     // загрузка окна
@@ -37,6 +42,40 @@ class AuthorizationForm extends Component {
         }
     }
 
+    handleChange = async (e) => {
+        const {target} = e;
+        this.setState({
+            [target.name]: target.value
+        });
+    }
+
+
+    submitForm = async (e) => {
+        e.preventDefault();
+        let data = { 
+            'username': this.state.login,
+            'password': this.state.password
+        };
+        console.log(data);
+        this.setState({
+            'password': ''
+        });
+        let url = Constants.LOGIN_PREFIX;
+        const response = await fetch(url, {
+            method: 'POST',
+            body: JSON.stringify(data),
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            credentials: "include"
+        });
+        console.log(response.headers);
+        if (response.status === 200) {
+            const tokens = await response.json();
+            console.log(tokens);
+        }      
+    }
+
     render() {
         return (
             <main>
@@ -46,17 +85,31 @@ class AuthorizationForm extends Component {
                     }}
                 >
                     <div className={'form-wrapper'}>
-                        <Form>
+                        <Form onSubmit={this.submitForm}>
                             <FormGroup row>
-                                <Label for="userEmail" sm={2}>Email</Label>
+                                <Label for="user_name" sm={2}>Логин</Label>
                                 <Col sm={10}>
-                                    <Input type="email" name="email" id="userEmail" placeholder="Введите email" />
+                                    <Input 
+                                        type="text" 
+                                        name="login"
+                                        id="user_name"
+                                        placeholder="Введите логин" 
+                                        value={this.state.login}
+                                        onChange={this.handleChange}
+                                    />
                                 </Col>
                             </FormGroup>
                             <FormGroup row>
                                 <Label for="userPassword" sm={2}>Password</Label>
                                 <Col sm={10}>
-                                    <Input type="password" name="password" id="userPassword" placeholder="Введите пароль" />
+                                    <Input 
+                                        type="password"
+                                        name="password" 
+                                        id="userPassword" 
+                                        placeholder="Введите пароль" 
+                                        value = {this.state.password}
+                                        onChange={this.handleChange}
+                                    />
                                 </Col>
                             </FormGroup>
                             <Button>Войти</Button>
--- frontend/src/constants/constants.js
+++ frontend/src/constants/constants.js
@@ -2,3 +2,5 @@
 export const BACKEND_PREFIX = 'http://127.0.0.1:8000';
 export const API_PREFIX = BACKEND_PREFIX + '/api';
 export const MAPS_PREFIX = API_PREFIX + '/maps/';
+export const AUTH_PREFIX = API_PREFIX + '/auth';
+export const LOGIN_PREFIX = AUTH_PREFIX + '/login/';
